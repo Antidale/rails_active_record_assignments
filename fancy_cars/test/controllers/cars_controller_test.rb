800548de4a96c3612afb39f82b42e04d2e035078
Scott Wyngarden <swyngarden@gmail.com>
2018-06-03 21:34:26 -0700

    added price to view, controller

--- fancy_cars/test/controllers/cars_controller_test.rb
+++ fancy_cars/test/controllers/cars_controller_test.rb
@@ -18,7 +18,7 @@ class CarsControllerTest < ActionController::TestCase
 
   test "should create car" do
     assert_difference('Car.count') do
-      post :create, car: { make: @car.make, model: @car.model, year: @car.year }
+      post :create, car: { make: @car.make, model: @car.model, year: @car.year, price: @car.price }
     end
 
     assert_redirected_to car_path(assigns(:car))
@@ -35,7 +35,7 @@ class CarsControllerTest < ActionController::TestCase
   end
 
   test "should update car" do
-    patch :update, id: @car, car: { make: @car.make, model: @car.model, year: @car.year }
+    patch :update, id: @car, car: { make: @car.make, model: @car.model, year: @car.year, price: @car.price}
     assert_redirected_to car_path(assigns(:car))
   end
 
