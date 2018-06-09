b525963d37097ba0e9aa425ab7aa533c82fdaa53
Scott Wyngarden <swyngarden@gmail.com>
2018-06-09 12:10:43 -0700

    adding seeds.rb

---
+++ advanced_ar/db/seeds.rb
@@ -0,0 +1,35 @@
+Person.destroy_all
+PersonalInfo.destroy_all
+Job.destroy_all
+
+(1..10).each do
+  person = Person.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    login: Faker::Internet.user_name(4..10),
+    pass: Faker::Internet.password(5,15)
+  )
+
+
+  person.create_personal_info(
+    height: Faker::Number.between(60,80),
+    weight: Faker::Number.between(120,250)
+  )
+
+  person.jobs.create! [
+    {
+      title: Faker::Job.title,
+      company: Faker::Company.name,
+      position_id: Faker::Number.between(1,100)
+    },{
+      title: Faker::Job.title,
+      company: Faker::Company.name,
+      position_id: Faker::Number.between(1,100)
+    },{
+      title: Faker::Job.title,
+      company: Faker::Company.name,
+      position_id: Faker::Number.between(1,100)
+    }
+  ]
+
+end
