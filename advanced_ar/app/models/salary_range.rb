f667263f9f8746745164b247b4804cc05b3f8172
Scott Wyngarden <swyngarden@gmail.com>
2018-06-09 15:38:38 -0700

    added a custom validation

--- advanced_ar/app/models/salary_range.rb
+++ advanced_ar/app/models/salary_range.rb
@@ -1,3 +1,10 @@
 class SalaryRange < ActiveRecord::Base
   belongs_to :job
+
+  validate :min_less_than_max
+
+  def min_less_than_max
+    if min_salary > max_salary
+      errors.add(:min_salary, "cannot be greater than maximum salary")
+  end
 end
