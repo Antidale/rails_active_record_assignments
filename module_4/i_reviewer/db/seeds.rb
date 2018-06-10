f0773c8a92570e66e11afe45745f171a928bc8cd
Scott Wyngarden <swyngarden@gmail.com>
2018-06-10 12:08:11 -0700

    added book creation to seeds.rb

--- module_4/i_reviewer/db/seeds.rb
+++ module_4/i_reviewer/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+Book.create! [
+  { name: "Iain M. Banks", author: "Matter" },
+  { name: "Ha'penny", author: "Jo Walton"},
+  { name: "Every Heart a Doorway", author: "Seanan McGuire"},
+  { name: "Memory", author: "Lois McMaster Bujold"},
+  { name: "Pattern Recognition", author: "William Gibson"},
+  { name: "A Song for Arbonne", author: "Guy Gavriel Kay"}
+]
