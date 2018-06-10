30aa42aed0228e6aaab9cc7e41f154a0ac5e09a6
Scott Wyngarden <swyngarden@gmail.com>
2018-06-10 13:45:39 -0700

    added some basic styling, fixed typo

--- module_4/i_reviewer/app/controllers/notes_controller.rb
+++ module_4/i_reviewer/app/controllers/notes_controller.rb
@@ -22,6 +22,6 @@ class NotesController < ApplicationController
     end
 
     def note_params
-      params.require(:note).permite(:title, :note)
+      params.require(:note).permit(:title, :note)
     end
 end
