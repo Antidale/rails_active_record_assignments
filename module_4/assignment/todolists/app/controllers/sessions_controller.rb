cb8fe1537c88a6ff7b0b6d587d3a6682dbdb2854
Scott Wyngarden <swyngarden@gmail.com>
2018-06-11 10:52:45 -0700

    all tests pass (well, the end-to-end one)

---
+++ module_4/assignment/todolists/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  skip_before_action :ensure_login, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:user][:username])
+    password = params[:user][:password]
+
+    if (user && user.authenticate(password))
+      session[:id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      redirect_to login_path, alert: "invalid username/password"
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to login_path, notice: "logout successful"
+  end
+end
